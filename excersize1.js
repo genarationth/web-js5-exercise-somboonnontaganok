159f25c6bbac45e3f664ad847bce507d91be8198
Somboon Nontaganok <125272647+somboonnontaganok@users.noreply.github.com>
2023-03-15 15:53:04 +0000

    Finish excercise 1

---
+++ excersize1.js
@@ -0,0 +1,9 @@
+let bankPeople = ['Sofia', 'David', 'Juan'];
+
+//  console.log(bankPeople); 
+bankPeople.push('Sara', 'Augustin.');
+// console.log(bankPeople);
+bankPeople.splice(2, 0, 'Renata');
+// console.log(bankPeople);
+bankPeople.push('Elena');
+console.log(bankPeople);
