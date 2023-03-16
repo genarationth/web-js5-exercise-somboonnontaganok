823f6a85cc72bb275627d17d63f353d4629bc11d
Somboon Nontaganok <125272647+somboonnontaganok@users.noreply.github.com>
2023-03-16 02:36:01 +0000

    finish excercise3

---
+++ excercise3.js
@@ -0,0 +1,32 @@
+let x = 5;
+while (x > 0)
+{
+    x -= 0.5;
+    console.log(x);
+}
+
+let y = 0;
+while (y < 100) 
+{
+    y += 1;
+    if (y % 2 != 0) {
+        console.log(y); 
+    }
+}
+
+let sum = 0;
+let n = 0;
+while (n < 6) 
+{
+   
+    sum += n;
+    n++;
+    
+}
+console.log(sum);
+
+let b = 0;
+while (b < 5) {
+    b++;
+    console.log([b]);
+}
